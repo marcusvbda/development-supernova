1d6ed30545c35bba03212dfc9da2ed236c12ad4f
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2024-02-13 02:10:14 -0300

    favicon

--- app/Http/Supernova/Application.php
+++ app/Http/Supernova/Application.php
@@ -6,8 +6,8 @@ use marcusvbda\supernova\Application as SupernovaApplication;
 
 class Application extends SupernovaApplication
 {
-    // public function secureRoutes()
-    // {
-    //     return false;
-    // }
+    public function icon(): string
+    {
+        return "favicon.png";
+    }
 }
