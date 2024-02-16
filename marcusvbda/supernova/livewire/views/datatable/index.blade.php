3ec7a50c1cb541d30833735013f5453b3ed89b18
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2024-02-16 15:50:42 -0300

    button indigo

--- marcusvbda/supernova/livewire/views/datatable/index.blade.php
+++ marcusvbda/supernova/livewire/views/datatable/index.blade.php
@@ -1,8 +1,13 @@
 <div class="flex flex-col items-center justify-center">
     <div class="flex flex-col gap-3 w-full">
-        @if ($searchable)
+        <div class="flex flex-row flex-wrap gap-3">
             @include('supernova-livewire-views::datatable.filter')
-        @endif
+            <div class="ml-auto">
+                <button class="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-6 rounded">
+                    Button
+                </button>
+            </div>
+        </div>
         @include('supernova-livewire-views::datatable.table')
     </div>
 </div>
